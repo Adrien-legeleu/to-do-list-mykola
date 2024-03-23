9b9083262886a94d9242e88e49d09998de9ff103
Test <adrienlegeleu@gmail.com>
2024-03-23 14:05:31 +0100

    to-do-list-reset

--- backend/server.ts
+++ backend/server.ts
@@ -1,32 +1,31 @@
-import express from "express";
-import mongoose from "mongoose";
-import appRouter from "./src/routes";
+import express from 'express';
+import mongoose from 'mongoose'
 import dotenv from "dotenv";
+import appRouter from './src/routes';
 
-dotenv.config();
+dotenv.config()
 
-const uri = process.env.MONGODB_URI || "";
-const PORT = 5060;
+const uri = process.env.MONGODB_URI
+const PORT = 5050;
 
-if (uri) {
-  mongoose
-    .connect(uri)
-    .then(() => {
-      console.log("DB connected !");
-    })
-    .catch((err) => console.log(err));
+if(uri) {
+    mongoose.connect(uri)
+        .then(() => {
+            console.log("DB connected !")
+        })
+        .catch((err) => console.log(err))
 } else {
-  console.log("No URI to DB");
+    console.log("No URI to DB")
 }
+
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json())
 
-app.use("/", appRouter);
+app.use('/', appRouter)
 
 app.listen(PORT, () => {
-  console.log(
-    `Server listen on port ${PORT} => url : http://localhost:${PORT}`
-  );
-});
+    console.log(`Server listen on port ${PORT} => url : http://localhost:${PORT}`)
+})
+
